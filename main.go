39ce06aad40a6c8b97f3ddf4defb08cab585a45c
maker2413 <maker2413@gmail.com>
2025-03-14 02:06:00 -0500

    began working on authentication

--- main.go
+++ main.go
@@ -48,11 +48,13 @@ func main() {
 	mux.Handle("/app/", apicfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 
+	mux.HandleFunc("POST /api/login", apicfg.handlerLogin)
 	mux.HandleFunc("POST /api/users", apicfg.handlerUsersCreate)
 	mux.HandleFunc("POST /api/chirps", apicfg.handlerChirpsCreate)
 	mux.HandleFunc("GET /api/chirps", apicfg.handlerChirpsRetrieve)
 	mux.HandleFunc("GET /admin/metrics", apicfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apicfg.handlerReset)
+	mux.HandleFunc("GET /api/chirps/{chirpID}", apicfg.handlerChirpsGet)
 
 	srv := http.Server{
 		Handler: mux,
