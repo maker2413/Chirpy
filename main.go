8c10fdba95e7d6cf15a8135bef6c38548ae966b8
maker2413 <maker2413@gmail.com>
2025-03-20 12:30:50 -0500

    added delete chirp functionality

--- main.go
+++ main.go
@@ -54,6 +54,8 @@ func main() {
 	mux.Handle("/app/", apicfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 
+	mux.HandleFunc("POST /api/polka/webhooks", apicfg.handlerWebhook)
+
 	mux.HandleFunc("POST /api/login", apicfg.handlerLogin)
 	mux.HandleFunc("POST /api/refresh", apicfg.handlerRefresh)
 	mux.HandleFunc("POST /api/revoke", apicfg.handlerRevoke)
@@ -64,6 +66,7 @@ func main() {
 	mux.HandleFunc("POST /api/chirps", apicfg.handlerChirpsCreate)
 	mux.HandleFunc("GET /api/chirps", apicfg.handlerChirpsRetrieve)
 	mux.HandleFunc("GET /api/chirps/{chirpID}", apicfg.handlerChirpsGet)
+	mux.HandleFunc("DELETE /api/chirps/{chirpID}", apicfg.handlerChirpsDelete)
 
 	mux.HandleFunc("GET /admin/metrics", apicfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apicfg.handlerReset)
